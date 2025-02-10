eb3389a3fff87dc40c84ba1a87bedf88005e2a0c
Cameron Forrest <cjforrest93@gmail.com>
2025-02-11 10:59:02 +1300

    Move logo to public

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
 
   return (
     <div className="flex items-center space-x-2 cursor-pointer" onClick={handleHomeClick}>
-      <img src="/src/assets/reddit-logo.svg" alt="Logo" className="h-8 w-8" />
+      <img src="/public/reddit-logo.svg" alt="Logo" className="h-8 w-8" />
       <h1 className="text-lg sm:text-xl font-bold text-white">
         Reddit Clone
       </h1>
